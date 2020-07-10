ceb81999fc3fd48f234a4fd81f65afca8b247c1a
Harry Sadler <harrysadlermusic@gmail.com>
2020-07-10 16:39:59 -0700

    update unity version for project, add a script for object dragging

---
+++ Assets/Scripts/TouchDragScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchDragScript : MonoBehaviour
+{
+
+    Collider2D col;
+    bool moveAllowed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = this.GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0) {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+            // handle touch phases
+            if(touch.phase == TouchPhase.Began) {
+                Collider2D touchedCol = Physics2D.OverlapPoint(touchPos);
+                if(col == touchedCol) {
+                    moveAllowed = true;
+                }
+            }
+            if(touch.phase == TouchPhase.Moved && moveAllowed) {
+                transform.position = new Vector2(touchPos.x, touchPos.y);
+            }
+            if(touch.phase == TouchPhase.Ended) {
+                moveAllowed = false;
+            }
+
+
+        }
+    }
+}
