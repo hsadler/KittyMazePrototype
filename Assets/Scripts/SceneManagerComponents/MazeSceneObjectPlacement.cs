d63019dc08ec50422100a9478a90840bfaf16705
Harry Sadler <harrysadlermusic@gmail.com>
2020-07-11 11:21:25 -0700

    add start and end point for maze, add scene managers, add runtime player placement

---
+++ Assets/Scripts/SceneManagerComponents/MazeSceneObjectPlacement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeSceneObjectPlacement {
+
+    public void PlacePlayerObject(GameObject playerObject, GameObject startPosition) {
+        GameObject.Instantiate(playerObject, startPosition.transform);
+    }
+    
+
+}
--- Assets/Scripts/SceneManagers/MazeSceneManager.cs
+++ Assets/Scripts/SceneManagers/MazeSceneManager.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class MazeSceneManager : MonoBehaviour {
 
 
-    public MazeSceneObjective mazeSceneObjective;
+	// scene managers
+	public MazeSceneObjective mazeSceneObjective;
+	public MazeSceneObjectPlacement mazeSceneObjectPlacement;
 
-    
-    // the static reference to the singleton instance
+	// scene objects
+	public GameObject playerObject;
+
+	
+	// the static reference to the singleton instance
 	public static MazeSceneManager instance { get; private set; }
 
 	// UNITY HOOKS
@@ -19,8 +24,27 @@ public class MazeSceneManager : MonoBehaviour {
 		} else {
 			Destroy(gameObject);
 		}
-        this.mazeSceneObjective = new MazeSceneObjective();
+		this.mazeSceneObjective = new MazeSceneObjective();
+		this.mazeSceneObjectPlacement = new MazeSceneObjectPlacement();
 	}
 
-    
+	void Start() {
+		PlacePlayerObject();
+	}
+
+	// INTERFACE METHODS
+
+	// IMPLEMENTATION METHODS
+
+	private void PlacePlayerObject() {
+		GameObject startPoint = GameObject.FindGameObjectWithTag("StartPoint");
+		var pos = new Vector3(
+			startPoint.transform.position.x, 
+			startPoint.transform.position.y, 
+			0
+		); 
+		GameObject.Instantiate(playerObject, pos, Quaternion.identity);
+	}
+
+	
 }
