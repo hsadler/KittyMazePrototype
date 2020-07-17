154cd55d64ac72b0cec8670f949232195e2e4330
Harry Sadler <harrysadlermusic@gmail.com>
2020-07-17 12:46:06 -0700

    get nav working from main-menu to maze, add some colors, add some buttons

---
+++ Assets/Scripts/SharedComponentScripts/SceneNavScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneNavScript : MonoBehaviour {
+
+
+    public void NavToMainMenuScene() {
+        SceneManager.LoadScene("MainMenuScene");
+    }
+
+    public void NavToMazeScene() {
+        SceneManager.LoadScene("MazeScene");
+    }
+
+    public void NavToDressUpScene() {
+        SceneManager.LoadScene("DressUpScene");
+    }
+
+
+}
