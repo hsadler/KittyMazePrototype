c15113a4b85e9b9148741b958b97b41d83c4f1cb
Harry Sadler <harrysadlermusic@gmail.com>
2020-07-22 14:11:50 -0700

    add notes, whiteboards, update TODOs, test load from unity Resources

---
+++ Assets/Scripts/GameObjectScripts/KittyScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KittyScript : MonoBehaviour {
+
+
+    public GameObject kittyContainer;
+    public GameObject bowContainer;
+    public GameObject scarfContainer;
+    public GameObject bootsContainer;
+
+
+    // UNITY HOOKS
+
+    void Start() {
+        // example of loading resources by name
+        GameObject bowBlue = Resources.Load<GameObject>("BowBlue");
+        Instantiate(bowBlue, this.gameObject.transform);
+    }
+
+    void Update() {}
+
+
+}
