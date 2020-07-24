e41381e59ecdbc949a89a0a22952982b24d57786
Harry Sadler <harrysadlermusic@gmail.com>
2020-07-24 12:20:28 -0700

    mock kitty-select and accessory-select scroll lists

---
+++ Assets/Scripts/GameObjectScripts/KittyAndAccessorySelectionScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KittyAndAccessorySelectionScript : MonoBehaviour {
+
+
+	public GameObject kittySelectScrollList;
+	public GameObject kittyAccessoriesScrollList;
+	
+
+	// UNITY HOOKS
+	
+	void Start() {}
+	void Update() {}
+
+	// INTERFACE METHODS
+
+	public void SwitchToKittySelectScrollList() {
+		kittyAccessoriesScrollList.SetActive(true);
+		kittyAccessoriesScrollList.SetActive(false);
+	}
+
+	public void SwitchToKittyAccessoriesScrollList() {
+		kittyAccessoriesScrollList.SetActive(false);
+		kittyAccessoriesScrollList.SetActive(true);
+	}
+
+
+}
