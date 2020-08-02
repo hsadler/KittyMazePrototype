3ddd09e7d5be73807d2de7ee852376b406e36e7d
Harry Sadler <harrysadlermusic@gmail.com>
2020-08-01 21:13:50 -0700

    poc kitty-asset -> data-model -> UI presentation

---
+++ Assets/Scripts/GameObjectScripts/KittyScrollContentItemScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KittyScrollContentItemScript : MonoBehaviour {
+
+
+    public Image kittyImage;
+
+    
+    void Start() {}
+
+    void Update() {}
+
+
+}
---
+++ Assets/Scripts/GameObjectScripts/KittySelectScrollListScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KittySelectScrollListScript : MonoBehaviour {
+
+
+	public GameObject scrollContent;
+	public GameObject kittyScrollContentItemPrefab;
+
+
+	void Start() {
+		List<KittyModel> kitties = Kitty.GetAllKitties();
+		foreach (var kittyModel in kitties) {
+			GameObject kittyScrollContentItem = Instantiate(
+				kittyScrollContentItemPrefab, 
+				scrollContent.transform
+			);
+			Sprite sprite = GameManager.instance.assets.GetSprite(kittyModel.assetName);
+			kittyScrollContentItem.GetComponent<KittyScrollContentItemScript>()
+				.kittyImage
+				.sprite = sprite;
+		}
+	}
+
+	void Update() {}
+
+
+}
