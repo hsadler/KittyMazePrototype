8516d602fe88a5b05d91df4150d5038ba7b0974a
Harry Sadler <harrysadlermusic@gmail.com>
2020-08-24 15:55:53 -0700

    more work setting up maze modal

---
+++ Assets/Scripts/GameObjectScripts/MazeModalScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeModalScript : MonoBehaviour {
+
+
+	// UNITY HOOKS
+
+	void Start() {}
+
+	void Update() {}
+
+
+}
